44449bef6e0b225b7248e0a4bbc0cd032a0f0524
cliffnyendwe <cliffnyendwe2018@gmail.com>
2021-02-03 19:06:41 +0000

    rearranged my project structure

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
-  /* size of various types on the computer */
   char c;
   int i;
   long li;
