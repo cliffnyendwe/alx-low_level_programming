46625c9ae1b530dcdddb8255e5b1efdb3189cebe
cliffnyendwe <cliffnyendwe2018@gmail.com>
2021-02-25 15:24:58 +0300

    corrected errors in task 0x02 of nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,15 +6,15 @@
  */
 int print_last_digit(int c)
 {
-  if (c > 0 || c == 0)
-    {
-      _putchar (c % 10 + '0');
-      return (c % 10);
-    }
-  else
-    {
-      c = c * -1;
-      _putchar (c % 10 + '0');
-      return (c % 10);
-    }
+	if (c > 0 || c == 0)
+	{
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
+	else
+	{
+	c = c * -1;
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
 }
